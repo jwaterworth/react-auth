62fd88f92ac132b65ad907d4f68d8ea67fc0ee02
jwaterworth <j.waterworth1990@gmail.com>
2016-10-20 21:53:04 +0100

    Reached '#Implementing Flux'

---
+++ src/components/Header.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { Nav, Navbar, NavItem, Header, Brand } from 'react-bootstrap';
+// import AuthActions from '../actions/AuthActions';
+// import AuthStore from '../stores/AuthStore';
+
+class HeaderComponent extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            authenticated: false
+        }
+
+        this.login = this.login.bind(this);
+        this.logout = this.logout.bind(this);
+    }
+
+    login() {
+        this.props.lock.show((err, profile, token) => {
+            if (err) {
+                alert(err);
+                return;
+            }
+
+            this.setState({ authenticated: true });
+        });
+    }
+
+    logout() {
+        this.setState({ authenticated: false });
+    }
+
+    render() {
+        return (
+            <Navbar>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <a href="#">React Contacts</a>
+                    </Navbar.Brand>
+                </Navbar.Header>
+                <Nav>
+                    <NavItem onClick={this.login}>Login</NavItem>
+                    <NavItem onClick={this.logout}>Logout</NavItem>
+                </Nav>
+            </Navbar>
+        );
+    }
+}
+
+export default HeaderComponent;
